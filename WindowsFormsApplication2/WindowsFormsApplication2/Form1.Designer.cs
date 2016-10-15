cacff1290a5777a982738dedc1497fac6ea42a82
davidbeal <dbeal55@gmail.com>
2016-10-15 15:32:25 +1100

    Update Form1.Designer.cs

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -49,7 +49,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "My Label";
             // 
             // Form1
             // 
